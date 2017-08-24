6e313e142727ababc45e34ea526a3bfa47fe4915
Anatoly Kolesov <kolesoffac@gmail.com>
2017-08-24 16:54:14 +0300

    full refactoring code, solved the problem with mutation

--- index.js
+++ index.js
@@ -1,37 +1,52 @@
-import each from "lodash-es/each";
+function checkChange(nextProp, prevProp, fnNext, fnPrev) {
+	const hasChanged = prevProp !== nextProp
+	const nextState = hasChanged ? fnNext && fnNext(nextProp) || nextProp : fnPrev && fnPrev(prevProp) || prevProp;
 
+	return nextState;
+};
+
+function getCombinations(combinations) {
+	return combinations || {};
+};
+//---------
+
+// #1
+export function getInitialState(combinations, initialState) {
+	let initialState$ = initialState;
+	let combinations$ = getCombinations(combinations);
+
+	Object.keys(combinations$).forEach(function(name) {
+		initialState$[name] = combinations$[name].module();
+	})
+
+	return initialState$;
+};
+
+// #2
 export function stateCombine(combinations) {
 	return function(prop, state, action) {
-		const combination = combinations[prop];
-		const { module } = combination;
+		const { module } = getCombinations(combinations)[prop] || {};
 
-		const prevProp = state[prop]
-		const nextProp = module(state[prop], action)
-		const hasChanged = prevProp !== nextProp
-		const nextState = hasChanged ? {
-			...state,
-			[prop]: nextProp,
-		} : state;
-
-		return nextState;
+		return module ? checkChange(module(state[prop], action), state[prop], function(nextProp) {
+			return {
+				...state,
+				[prop]: nextProp,
+			}
+		}, function(prevProp) {
+			return state;
+		}) : state;
 	};
 };
 
+// #3
 export function runCombine(combinations, combine) {
 	return function(state, action) {
-		// TODO: the same problem.
-		// We should not mutate state from scope rathen then return from function.
-		each(combinations, function(combination, name) {
-			if (combination.actions.includes(action.type)) {
-				state = combine(name, state, action);
-			};
+		let prevProp = state;
+
+		Object.keys(getCombinations(combinations)).forEach(function(name) {
+			state = combine(name, state, action);
 		});
+
+		return checkChange(state, prevProp);
 	};
-};
-
-export function getInitialState(combinations, initialState) {
-	const combineInitial = (memo, [combination, name]) =>
-		Object.assign(memo, { [name]: combination.module() });
-
-	return Object.entries(combinations).reduce(combineInitial, initialState)
 };
